a3c62fad91dec811104930da560e80f6006b8e16
Steven Bryant <stevelad_83@yahoo.com>
2023-01-24 17:15:31 -0500

    added styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Nav from '../Nav/Nav.js';
 import Display from '../Display/Display.js';
 
 export default function Main() {
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,19 +4,21 @@ import { NavLink } from 'react-router-dom';
 export default function Nav() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/255/102/102">Red</NavLink>
-          </li>
-          <li>
-            <NavLink to="/102/255/178">Green</NavLink>
-          </li>
-          <li>
-            <NavLink to="/153/153/255">Blue</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="nav-container">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/255/102/102">Red</NavLink>
+            </li>
+            <li>
+              <NavLink to="/102/255/178">Green</NavLink>
+            </li>
+            <li>
+              <NavLink to="/153/153/255">Blue</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }

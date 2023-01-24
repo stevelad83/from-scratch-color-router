e9839c6d8f2d282916dd347c5f191c5f71b3f88e
Steven Bryant <stevelad_83@yahoo.com>
2023-01-24 17:37:28 -0500

    more stylimg

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Display.css';
 
 export default function Display() {
   const { r, g, b } = useParams();
   return (
-    <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b}` }}>
+    <div className="display-container" style={{ backgroundColor: `rgb(${r}, ${g}, ${b}` }}>
       <p>
         rgb({r}, {g}, {b})
       </p>
